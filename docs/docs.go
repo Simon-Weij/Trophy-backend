b03bcf18e264a0fea8f0b1f675554f2427995d4a
Simon-Weij <175155691+Simon-Weij@users.noreply.github.com>
2026-04-10 07:16:48 +0000

    docs: regenerate

--- docs/docs.go
+++ docs/docs.go
@@ -448,7 +448,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "127.0.0.1:3000",
+	Host:             "localhost:3000",
 	BasePath:         "/api",
 	Schemes:          []string{"http"},
 	Title:            "Trophy API",
